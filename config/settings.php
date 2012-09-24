4c1ab645ad5616659590ff257d4cea85f9b5bc98
Fabian von Gadomski <fvg@cube14.net>
2012-09-24 23:39:00 +0200

    don't need DS

--- config/settings.php
+++ config/settings.php
@@ -2,12 +2,12 @@
 
 return array(
 	'script_url' => URL::to_route('upload'),
-	'upload_dir' => path('public').'bundles/jupload/uploads'.DS.'files/',
+	'upload_dir' => path('public').'bundles/jupload/uploads/files/',
 	'upload_url' => URL::base().'/bundles/jupload/uploads/files/',
 	'delete_type' => 'POST',
 	'image_versions' => array(
 		'thumbnail' => array(
-			'upload_dir' => path('public').'bundles/jupload/uploads'.DS.'thumbnails/',
+			'upload_dir' => path('public').'bundles/jupload/uploads/thumbnails/',
 			'upload_url' => URL::base().'/bundles/jupload/uploads/thumbnails/',
 		),
 	),
