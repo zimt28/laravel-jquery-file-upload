fc18b457bedf1fae183b7e0f3a570f7cfede053d
Fabian von Gadomski <fvg@cube14.net>
2012-09-25 15:24:14 +0200

    Headers? No direct access

--- routes.php
+++ routes.php
@@ -14,13 +14,16 @@ Route::any('(:bundle)/upload', array('as' => 'upload', function()
 {
 	$upload_handler = IoC::resolve('UploadHandler');
 
-	header('Pragma: no-cache');
+	if ( ! Request::ajax())
+		return;
+
+/*  header('Pragma: no-cache');
 	header('Cache-Control: no-store, no-cache, must-revalidate');
 	header('Content-Disposition: inline; filename="files.json"');
 	header('X-Content-Type-Options: nosniff');
 	header('Access-Control-Allow-Origin: *');
 	header('Access-Control-Allow-Methods: OPTIONS, HEAD, GET, POST, PUT, DELETE');
-	header('Access-Control-Allow-Headers: X-File-Name, X-File-Type, X-File-Size');
+	header('Access-Control-Allow-Headers: X-File-Name, X-File-Type, X-File-Size'); */
 
 	switch (Request::method())
 	{
@@ -39,9 +42,6 @@ Route::any('(:bundle)/upload', array('as' => 'upload', function()
 			{
 				$upload_handler->post();
 			}
-			break;
-		case 'DELETE':
-			$upload_handler->delete();
 			break;
 		default:
 			header('HTTP/1.1 405 Method Not Allowed');
